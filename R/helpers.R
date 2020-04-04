513c6335a3fe8f6ffba414f59853fef3a80b6e4d
Babatunde Alli <babatunde.alli@mail.mcgill.ca>
2020-04-04 17:06:45 -0400

    Created helper function for the boot function

--- R/helpers.R
+++ R/helpers.R
@@ -6,3 +6,22 @@ invalid <- function(model) {
   cls <- c("glm", "coxph", "clogit", "lm")
   return(class(model)[1] %in% cls)
 }
+
+#for bstrap
+trio = function(obj){
+
+  exp1 <- names(coef(obj))[2]
+  exp2 <- names(coef(obj))[3]
+  inta <- paste(exp1, exp2, sep = ":")
+
+  b1 <- coef(obj)[exp1]
+  b2 <- coef(obj)[exp2]
+  b3 <- coef(obj)[inta]
+
+  OR10 <- exp(b1)
+  OR01 <- exp(b2)
+  OR11 <- exp(b1 + b2 + b3)
+  out = c(RERI = OR11 - OR10 - OR01 + 1, AP = (OR11 - OR10 - OR01 + 1) / OR11, SI = (OR11 -
+                                                                                       1) / (OR10 + OR01 - 2))
+  return(out)
+}
