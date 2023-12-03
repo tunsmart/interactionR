cd0a5959bbda57762c2d5f7c52c3295cb992462c
Babatunde Alli <babatunde.alli@mail.mcgill.ca>
2023-12-03 12:11:26 -0500

    Add plnorm to globals

--- R/global.R
+++ R/global.R
@@ -1,5 +1,5 @@
 utils::globalVariables(c(
   "stats", "binomial", "coef", "confint.default", "glm",
   "pnorm", "qnorm", "quantile", "update", "vcov", "askYesNo", "grep",
-  "str2lang", "model.frame", "is"
+  "str2lang", "model.frame", "is", "plnorm"
 ))
